59666353476cc026eec033047f13187af0ea524b
GiovanniPineapple <gi.pina@duocuc.cl>
2024-11-04 13:07:00 -0300

    Delete js directory

--- js/servicio.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('serviceForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita que el formulario se envíe de manera predeterminada
-
-    // Aquí puedes agregar la lógica para enviar los datos a tu backend
-    alert("Servicio creado exitosamente.");
-});
